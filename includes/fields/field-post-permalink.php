ea760e6eeafbc1f3f673601afa34e5d655c220ec
ACF-Extended <contact@acf-extended.com>
2020-06-19 17:22:45 +0200

    Post Fields

---
+++ includes/fields/field-post-permalink.php
@@ -0,0 +1,53 @@
+<?php
+
+if(!defined('ABSPATH'))
+    exit;
+
+if(!class_exists('acfe_field_post_permalink')):
+
+class acfe_field_post_permalink extends acf_field{
+    
+    function __construct(){
+        
+        $this->name = 'acfe_post_permalink';
+        $this->label = __('Post Permalink', 'acfe');
+        $this->category = 'Post';
+        $this->defaults = array();
+        
+        parent::__construct();
+        
+    }
+    
+    function load_field($field){
+        
+        $field['name'] = '';
+        $field['required'] = 0;
+        $field['value'] = false;
+        
+        return $field;
+        
+    }
+    
+    function prepare_field($field){
+    
+        $post_id = acf_get_valid_post_id();
+    
+        if(!$post_id)
+            return false;
+        
+        $data = acf_get_post_id_info($post_id);
+    
+        // Bail early if not Post
+        if($data['type'] !== 'post')
+            return false;
+        
+        return $field;
+    
+    }
+    
+}
+
+// initialize
+acf_register_field_type('acfe_field_post_permalink');
+
+endif;
